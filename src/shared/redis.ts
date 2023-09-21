db4fa24994274bc82e53680816b463614ed48277
Md Razwan Niam <razwanniam1@gmail.com>
2023-09-21 19:58:07 +0600

    core service redis integreted

--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -1,67 +1,71 @@
-import { SetOptions, createClient } from "redis";
-import config from "../config";
+import { SetOptions, createClient } from 'redis';
+import config from '../config';
 
 const redisClient = createClient({
-    url: config.redis.url
+  url: config.redis.url,
 });
-
 const redisPubClient = createClient({
-    url: config.redis.url
+  url: config.redis.url,
 });
-
 const redisSubClient = createClient({
-    url: config.redis.url
+  url: config.redis.url,
 });
 
+redisClient.on('error', error => console.log('RedisErrror', error));
+
+redisClient.on('connect', () => console.log('Redis Connected'));
 
 const connect = async (): Promise<void> => {
-    await redisClient.connect();
-    await redisPubClient.connect();
-    await redisSubClient.connect();
+  await redisClient.connect();
+  await redisPubClient.connect();
+  await redisSubClient.connect();
 };
 
-const set = async (key: string, value: string, options?: SetOptions): Promise<void> => {
-    await redisClient.set(key, value, options);
+const set = async (
+  key: string,
+  value: string,
+  options?: SetOptions
+): Promise<void> => {
+  await redisClient.set(key, value, options);
 };
 
 const get = async (key: string): Promise<string | null> => {
-    return await redisClient.get(key);
+  return await redisClient.get(key);
 };
-
 const del = async (key: string): Promise<void> => {
-    await redisClient.del(key);
+  await redisClient.del(key);
 };
 
-const setAccessToken = async (userId: string, token: string): Promise<void> => {
-    const key = `access-token:${userId}`;
-    await redisClient.set(key, token, { EX: Number(config.redis.expires_in) })
-}
-
-const getAccessToken = async (userId: string): Promise<string | null> => {
-    const key = `access-token:${userId}`;
-    return await redisClient.get(key)
+const setAccessToken = async (userid: string, token: string): Promise<void> => {
+  const key = `access-token:${userid}`;
+  await redisClient.set(key, token, { EX: Number(config.redis.expires_in) });
 };
 
-const delAccessToken = async (userId: string): Promise<void> => {
-    const key = `access-token:${userId}`;
-    await redisClient.del(key);
-}
+const getAccessToken = async (userid: string): Promise<string | null> => {
+  const key = `access-token:${userid}`;
+  return await redisClient.get(key);
+};
+
+const deleteAccessToken = async (userid: string): Promise<void> => {
+  const key = `access-token:${userid}`;
+  await redisClient.del(key);
+};
 
 const disconnect = async (): Promise<void> => {
-    await redisClient.quit();
-    await redisPubClient.quit();
-    await redisSubClient.quit();
-}
+  await redisClient.quit();
+  await redisPubClient.quit();
+  await redisSubClient.quit();
+};
 
 export const RedisClient = {
-    connect,
-    set,
-    get,
-    del,
-    setAccessToken,
-    getAccessToken,
-    delAccessToken,
-    disconnect,
-    publish: redisPubClient.publish.bind(redisPubClient),
-    subscribe: redisSubClient.subscribe.bind(redisSubClient)
-}
+  set,
+  get,
+  del,
+  disconnect,
+  getAccessToken,
+  setAccessToken,
+  deleteAccessToken,
+  connect,
+  publish: redisPubClient.publish.bind(redisPubClient),
+  subscribe: redisSubClient.subscribe.bind(redisSubClient),
+};
